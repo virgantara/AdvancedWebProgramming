1046770b5dac4b546213999b47f7e2e116a9a27b
virgantara <oddy@unida.gontor.ac.id>
2024-10-12 10:50:59 +0700

    + arrow func 2

---
+++ week2/arrow_func.js
@@ -0,0 +1,3 @@
+ let myFunction = (a, b) => a * b; 
+
+ console.log(myFunction(2,3))
--- week2/index.php
+++ week2/index.php
@@ -2,3 +2,17 @@
 <h1>Week 2</h1>
 
 <h3>Modern Javascript ES6+</h3>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 2 - Modern JavaScript ES6+</title>
+</head>
+<body>
+    
+
+    <script type="module" src="arrow_func.js"></script>
+</body>
+</html>
